1a2eaff83269ee80649df4be5b9206c5ce282aba
charlewn <charleswchng@gmail.com>
2017-09-05 17:31:54 +0800

    add

---
+++ gae_limiter.py
@@ -0,0 +1,31 @@
+import logging
+# from functools import wraps
+import functools
+from controllers.Handler import Handler
+import webapp2
+
+class Limiter(object):
+	"""docstring for Limiter"""
+	# limit = staticmethod( limit )
+	def __init__(self):
+		super(Limiter, self).__init__()
+		# pass
+		# self.arg = arg
+		
+	def limit(self, limit_request, interval, per):
+		"""
+		Decorator to be use
+		:param limit_value: the limit request number
+		:param interval: num of years, months, days etc...
+		:param interval_unit: str, year, month, day, hour, minute
+		"""
+		def actual_decorator(func):
+			
+			@functools.wraps(func)
+			def wrapper(*args, **kwargs):
+				# all limiter logic here
+				logging.info(per)
+					
+				return func(*args, **kwargs)
+			return wrapper
+		return actual_decorator
